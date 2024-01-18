c8a48165ec5898ff26276a1065d51f2ad20ec455
Dianettt <143523569+Dia-net@users.noreply.github.com>
2024-01-19 00:33:55 +0200

    task5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColor = document.querySelector('.change-color');
+const element = document.body;
+const color = document.querySelector(".color");
+changeColor.addEventListener('click', function(){
+  const newColor = getRandomHexColor();
+  element.style.backgroundColor = newColor;
+  color.textContent = newColor;
+})
+
+
